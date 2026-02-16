ec832541d2b22299aac9a17cec5237b48fc4276e
TiaryXD <tiaryandraina65@gmail.com>
2026-02-16 19:06:43 +0300

    tsy haiko raha mety e(historique dons)

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\controllers\DonController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -62,6 +63,11 @@ $router->group('', function(Router $router) use ($app) {
     $router->get('/villes/{id}', function($id) use ($app) {
         BesoinController::showVilleById($app, $id);
     });
+
+        $router->get('/dons', function() use ($app) {
+        DonController::historique($app);
+    });
+     
     
 
 }, [ SecurityHeadersMiddleware::class ]);
